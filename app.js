21f9789079c166f6717c3837ca6282acc4aef13d
unknown <mohanad.13.93@gmail.com>
2023-06-16 01:04:54 +0200

    enable cors

--- app.js
+++ app.js
@@ -27,6 +27,35 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'doctor_images')))
 
 
+// enable cors on Vercel
+
+
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = (req, res) => {
+  const d = new Date()
+  res.end(d.toString())
+}
+
+module.exports = allowCors(handler)
+
+
+
 // connecting with DB
 var mongoos = require('mongoose');
 const config = require('./config');
